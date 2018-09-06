fff922a267f61ef7c0f1d0459cdd41da3316e60a
tanzhiwei2356 <981119228@qq.com>
2018-09-06 21:45:41 +0800

    初始化项目

---
+++ test/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by 谭志伟 on 2018/9/6.
+//  Copyright © 2018 谭志伟. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
